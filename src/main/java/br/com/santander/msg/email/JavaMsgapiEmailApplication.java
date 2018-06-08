49ee51ccd1895fa3750695f04482eebb5889d7a9
Lucas Cruz Fabiano <lucas.cruz.fabiano@everis.com>
2018-06-08 09:57:26 -0300

    retirando codigo comentado

--- src/main/java/br/com/santander/msg/email/JavaMsgapiEmailApplication.java
+++ src/main/java/br/com/santander/msg/email/JavaMsgapiEmailApplication.java
@@ -1,12 +1,7 @@
 package br.com.santander.msg.email;
 
-import java.util.Properties;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @SpringBootApplication
 public class JavaMsgapiEmailApplication {
@@ -15,21 +10,4 @@ public class JavaMsgapiEmailApplication {
 		SpringApplication.run(JavaMsgapiEmailApplication.class, args);
 	}
 	
-	/*@Bean
-	public JavaMailSender getJavaMailSender() {
-	    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-	    mailSender.setHost("smtp.gmail.com");
-	    mailSender.setPort(587);
-	     
-	    mailSender.setUsername("lucasguaruja@gmail.com");
-	    mailSender.setPassword("Msneom05%");
-	     
-	    Properties props = mailSender.getJavaMailProperties();
-	    props.put("mail.transport.protocol", "smtp");
-	    props.put("mail.smtp.auth", "true");
-	    props.put("mail.smtp.starttls.enable", "true");
-	    props.put("mail.debug", "true");
-	     
-	    return mailSender;
-	}*/
 }
